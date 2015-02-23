071ad5eac7de44491b32735fc200523a2119bffd
luissifu@xubuntu <luis.e.sifuentes@gmail.com>
2015-02-22 23:32:21 -0600

    added creational script and db connection

---
+++ scripts/db.sql
@@ -0,0 +1,21 @@
+create database practicafb;
+
+use practicafb;
+
+create table User (
+  id int AUTO_INCREMENT,
+  name varchar(100),
+  primary key (id)
+);
+
+create table Post (
+  id int AUTO_INCREMENT,
+  post varchar(500),
+  user_id int,
+  created_at timestamp,
+  primary key (id),
+  foreign key (user_id) references User(id)
+);
+
+insert into User (name)
+  values ("Luis Sifuentes");
