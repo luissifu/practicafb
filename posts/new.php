6c882af9b0104b92c487451cdd11eb61626deaed
luissifu@laptop <luis.e.sifuentes@gmail.com>
2015-02-26 21:49:13 -0600

    added some more protection against sql injection

--- posts/new.php
+++ posts/new.php
@@ -7,8 +7,6 @@ if (!$error)
 $response;
 if ($_POST && isset($_POST['post']))
 {
-  
-
   $post_content = $_POST['post'];
 
   $query = "INSERT INTO Post (post, user_id, created_at) VALUES ('$post_content', 1, NOW());";
@@ -17,7 +15,7 @@ if ($_POST && isset($_POST['post']))
   if (!$result)
   {
     http_response_code(503);
-    $response = array("message" => "Error en el servidor");
+    $response = array("message" => "Error en el servidor", "info" => mysqli_error($connection));
   }
   else
   {
